b28c5579f488bd6ea8e0aa1b51a271a8a6a8fad7
five2digitals <it.fivetwodigitals@gmail.com>
2026-08-29 21:27:47 +0530

    Add new functionality and Prisma updates

---
+++ ecommerce-backend/prisma/migrations/20260829093621_in/migration.sql
@@ -0,0 +1,14 @@
+-- DropIndex
+DROP INDEX "categories_name_trgm_idx";
+
+-- DropIndex
+DROP INDEX "customers_email_trgm_idx";
+
+-- DropIndex
+DROP INDEX "products_name_trgm_idx";
+
+-- DropIndex
+DROP INDEX "products_sku_trgm_idx";
+
+-- DropIndex
+DROP INDEX "products_tags_idx";
